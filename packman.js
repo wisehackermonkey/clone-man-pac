a8bf24179e4fd068cb4af0583b6b8b725dc0f20b
Oran C <oranbusiness@gmail.com>
2018-09-11 22:14:41 -0700

    tile map, collision(broken)

--- packman.js
+++ packman.js
@@ -1,13 +1,16 @@
 
 // character movement and drawing class
-function Character(x,y){
+function Character(x,y,tiles){
   this.pos = createVector(x,y);
+  this.tiles = tiles;
   
-  this.w = 100;
-  this.h = 100;
+  this.w = 30;
+  this.h = 30;
+  this.r = this.w;
   this.dirs = {"UP":createVector(0,-1),"DOWN":createVector(0,1),"RIGHT":createVector(1,0),"LEFT":createVector(-1,0),"STOP":createVector(0,0)};
   this.direction = "";
   
+  this.curTile = createVector(1,1);
   
   this.show = function(){
     // rect(this.pos.x, this.pos.y, this.w,this.h);
@@ -27,7 +30,13 @@ function Character(x,y){
       this.direction = "LEFT";
     }
     if(keyIsDown(UP_ARROW)){
+      var c = this.curTile.copy();
       this.direction = "UP";
+      this.calcCurrentTile();
+      if( this.tiles[c.x][c.y - 1] === 0){
+        this.direction = "STOP";
+        fill(color("green"));
+      }
     }
     if(keyIsDown(DOWN_ARROW)){
       this.direction = "DOWN";
@@ -37,8 +46,24 @@ function Character(x,y){
   this.move = function(){
     this.position(this.dirs[this.direction]);
   }
+  this.setDir = function(dir){
+    this.direction = dir;
+  }
+  //https://github.com/bmoren/p5.collide2D#colliderectcircle
+  this.detectCircle = function(box){
+    var w = box.w;
+    var h = box.h;
+    var x = box.pos.x;
+    var y = box.pos.y;
+    var result = collideRectCircle(x,y,w,h, this.pos.x,this.pos.y,this.r);
+    return result;
+  }
   
   this.movePtAtoB = function(){
     
   } 
+  this.calcCurrentTile= function(){
+    this.curTile
+  }
+  
 }
--- sketch.js
+++ sketch.js
@@ -10,23 +10,115 @@ github.com/wisehackermonkey
 
 */
 var packman;
-var myAngle = 30;
-
+var myAngle = 10;
+var size;
 //enums for directions
+var gui;
+var tileLayout;
+var walls = [];
+// Helper functions
+
+// transpose(matrix)
+// https://stackoverflow.com/a/13241545/5460870
+p5.prototype.transpose = function (a) {
+    return Object.keys(a[0]).map(function(c) {
+        return a.map(function(r) { return r[c]; });
+    });
+}
 
 function setup() {
   createCanvas(600,600);
   background(50);
-  packman = new Character(0,300);
+  size = 33;
   angleMode(DEGREES);
+  noStroke();
+  
+  // packman.w = size;
+  // packman.h = size;
+  
+  gui = createGui("Packman", 550,10);
+  gui.addGlobals('size');
+  
+  
+  // tileLayout = [
+  //   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  //   [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  //   [1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1],
+  //   [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  //   [1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1],
+  //   [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+  //   [1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1],
+  //   [1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1],
+  //   [1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1],
+  //   [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+  // ];
+  
+  tileLayout = [
+    [1, 1, 1],
+    [0, 0, 0],
+    [0, 0, 0],
+ 
+  ];
+ 
+  packman = new Character(1*size+30/2,1*size+30/2,tileLayout);
+  tileLayout = transpose(tileLayout);
+  var nbeginX  = 0;
+  var nbeginY  = 0;
+  var nwidth = tileLayout.length;
+  var nheight = tileLayout[0].length;
+  
+  for (var i = nbeginX,x=0; i < nwidth * size; i += size,x++) {
+    for (var j = nbeginY,y=0; j < nheight * size; j += size,y++) {
+     if(tileLayout[x][y] === 1){
+       noStroke();
+      rect(i, j, size - 1, size - 1);
+      stroke(1);
+      walls.push(new Wall(i,j,size -1,size -1));
+
+     }
+    }
+  }
 }
 
 function draw() {
     background(50);
-
+  
+  // var nwidth = 17;//width/size;
+  // var nheight = 19;//height/size;
+  for(var i = 0; i < walls.length; i+=1){
+    if(packman.detectCircle(walls[i])){
+      fill(color("red"));
+      // packman.setDir("STOP");
+      // if(packman.direction === "UP"){
+      //   packman.direction = "STOP";
+      // }
+      // print(packman.pos)
+    }
+    walls[i].show();
+    
+  }
+  
+  // for(var i = nbeginX; i < nwidth * size; i+=size){
+  //   for(var j = nbeginy; j < nheight * size; j+=size){
+  //   	rect(i,j,size-1,size-1);
+  //   }
+  // }
   packman.key_input();
   packman.move();
 
   packman.show();
 }
 
+
+function Wall(x,y,w,h){
+  this.pos = createVector(x,y);
+  this.w = w;
+  this.h = h;
+  this.show = function(){
+    rect(this.pos.x, this.pos.y, this.w, this.h);
+  }
+}
+
+function mousePressed(){
+  
+}
